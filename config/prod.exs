1f6f1d8ec028bb0c2868908fa8e7e9d76db838b2
Amer Ansari <amer@savandbros.com>
2022-11-08 23:36:38 +0400

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -12,6 +12,7 @@ import Config
 config :chatbaz, ChatbazWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "jubilant-lavender-bedbug.com", port: 80],
+  check_origin: ["jubilant-lavender-bedbug.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
